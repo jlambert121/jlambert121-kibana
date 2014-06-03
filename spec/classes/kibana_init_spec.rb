faa71b2f9dc66b84ebe45af569863d36cee81e61
Justin Lambert <jlambert@eml.cc>
2014-06-03 06:28:10 -0600

    update for kibana 3.1.0

--- spec/classes/kibana_init_spec.rb
+++ spec/classes/kibana_init_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'kibana', :type => :class do
   let(:facts) { { :concat_basedir => '/var/lib/puppet/concat' } }
 
-  it { should include_class('kibana::package') }
-  it { should include_class('kibana::config') }
+  it { should contain_class('kibana::package') }
+  it { should contain_class('kibana::config') }
 
 end
 
--- templates/config.js
+++ templates/config.js
@@ -8,30 +8,10 @@ function (Settings) {
 
   return new Settings({
 
-    /**
-     * URL to your elasticsearch server. You almost certainly don't
-     * want 'http://localhost:9200' here. Even if Kibana and ES are on
-     * the same host
-     *
-     * By default this will attempt to reach ES at the same host you have
-     * elasticsearch installed on. You probably want to set it to the FQDN of your
-     * elasticsearch host
-     * @type {String}
-     */
     elasticsearch: '<%= scope.lookupvar('es_real') %>',
-
-    /**
-     * The default ES index to use for storing Kibana specific object
-     * such as stored dashboards
-     * @type {String}
-     */
+    default_route: '/dashboard/file/default.json',
     kibana_index: "kibana-int",
-
-    /**
-     * Panel modules available. Panels will only be loaded when they are defined in the
-     * dashboard, but this list is used in the "add panel" interface.
-     * @type {Array}
-     */
     panel_names: ['<%= scope.lookupvar('modules').join("','") -%>'],
+
   });
 });
