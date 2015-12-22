b835a57ed9d9758049678adee089a7fdf2795736
Daniel Jensen <jensen@origamilogic.com>
2015-12-22 14:54:56 -0600

    fix lint erorr

--- manifests/install.pp
+++ manifests/install.pp
@@ -41,7 +41,7 @@ class kibana::install (
     source      => "${base_url}/${filename}.tar.gz",
     destination => "${tmp_dir}/${filename}.tar.gz",
     require     => User[$user],
-    unless      => "test -e ${install_path}/${filename}/LICENSE.txt"
+    unless      => "test -e ${install_path}/${filename}/LICENSE.txt",
   }
 
   exec { 'extract_kibana':
